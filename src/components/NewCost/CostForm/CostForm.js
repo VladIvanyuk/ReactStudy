c87bcaa7a2a17bda913b8d9af18f5998904a702c
Vladislav Ivanyuk <vl.ivanyuk1994@gmail.com>
2023-04-06 18:45:24 +1000

    стейты

--- src/components/NewCost/CostForm/CostForm.js
+++ src/components/NewCost/CostForm/CostForm.js
@@ -3,27 +3,73 @@ import { useState } from 'react';
 
 export const CostForm = (props) => {
 
-  const [name, setName] = useState('');
+  const [inputName, setInputName] = useState('');
+  const [inputAmount, setInputAmount] = useState('');
+  const [inputDate, setInputDate] = useState('');
+
+  // const [userInput, setUserInput] = useState({
+  //   name: '',
+  //   amount: '',
+  //   date: '',
+  // })
 
   const nameChangeHandler = (e) => {
-    console.log(e);
-    setName(e.target.value);
+    setInputName(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   name: e.target.value,
+    // })
+    // setUserInput((prev) => {
+    //   return {
+    //     ...prev,
+    //     name: e.target.value
+    //   }
+    // })
+  };
+
+  const amountChangeHandler = (e) => {
+    setInputAmount(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: e.target.value,
+    // })
+  };
+
+  const dateChangeHandler = (e) => {
+    setInputDate(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   date: e.target.value,
+    // })
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const costData = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate)
+    }
+
+    setInputName('');
+    setInputAmount('');
+    setInputDate('');
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className={styles.controls}>
         <div className={styles.control}>
           <label>Название</label>
-          <input type="text" onChange={nameChangeHandler}/>
+          <input type="text" value={inputName} onChange={nameChangeHandler}/>
         </div>
         <div className={styles.control}>
           <label>Сумма</label>
-          <input type="number" min='0.01' step='0.01'/>
+          <input type="number" min='0.01' step='0.01' value={inputAmount} onChange={amountChangeHandler}/>
         </div>
         <div className={styles.control}>
           <label>Дата</label>
-          <input type="date" min='2019-01-01' max='2024-01-01'/>
+          <input type="date" min='2019-01-01' max='2024-01-01' value={inputDate} onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className={styles.actions}>
